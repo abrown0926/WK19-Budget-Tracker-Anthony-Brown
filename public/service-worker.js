45c2639c338475320f1cba6a8cf739057d5e3a7f
AQB <abrown0926>
2021-12-12 12:33:17 -0600

    updated service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,25 +1,34 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
+  "/",
   "/index.html",
-  "/styles.css",
-  "/db.js",
+  "/favicon.ico",
+  "/manifest.webmanifest",
+  "/style.css",
   "/index.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
-  "/manifest.webmanifest",
 ];
 
-const CACHE_NAME = "static-cache-v3";
-const DATA_CACHE_NAME = "data-cache-v1";
-
+// install
 self.addEventListener("install", function (evt) {
+  // pre cache image data
   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Your files were pre-cached successfully!");
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/icons"))
   );
+
+  // pre cache all static assets
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  // tell the browser to activate this service worker immediately once it
+  // has finished installing
   self.skipWaiting();
 });
+
+// activate
 self.addEventListener("activate", function (evt) {
   evt.waitUntil(
     caches.keys().then((keyList) => {
@@ -33,50 +42,41 @@ self.addEventListener("activate", function (evt) {
       );
     })
   );
+
   self.clients.claim();
 });
+
 // fetch
-self.addEventListener("fetch", (evt) => {
-  // non GET requests are not cached and requests to other origins are not cached
-  if (
-    evt.request.method !== "GET" ||
-    !evt.request.url.startsWith(self.location.origin)
-  ) {
-    evt.respondWith(fetch(evt.request));
-    return;
-  }
-
-  // handle runtime GET requests for data from /api routes
-
-  if (evt.request.url.includes("/api/transaction")) {
-    // make network request and fallback to cache if network request fails (offline)
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/")) {
     evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then((cache) => {
-        return fetch(evt.request)
-          .then((response) => {
-            cache.put(evt.request, response.clone());
-            return response;
-          })
-          .catch(() => caches.match(evt.request));
-      })
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
     );
+
     return;
   }
 
-  // use cache first for all other requests for performance
   evt.respondWith(
-    caches.match(evt.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      // request is not in cache. make network request and cache the response
-      return caches.open(DATA_CACHE_NAME).then((cache) => {
-        return fetch(evt.request).then((response) => {
-          return cache.put(evt.request, response.clone()).then(() => {
-            return response;
-          });
-        });
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
       });
     })
   );
--- server.js
+++ server.js
@@ -15,10 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log(err));
 
 // routes
 app.use(require("./routes/api.js"));
